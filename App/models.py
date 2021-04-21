716a356d7b9244f4afd79c63e1ad68e24e3fe99b
NikkolaiLeung <nikkolai.leung@my.uwi.edu>
2021-04-21 05:22:42 +0000

    1 (#2)

---
+++ App/models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserLog
+db = SQLAlchemy()
+
+class User(db.Model, UserLog,):
+    id = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, nullable=False, unique=True)
+    email = db.Column(db.String(300), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    def toDict(self):
+        return{
+            "id": self.id,
+            "User Id": self.userId,
+            "Email:": self.email,
+            "password": self.password
+        } 
+        
+        
+ # hashes the password parameter and stores it in the object
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
